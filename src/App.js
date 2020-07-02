13bc8d67de53c4bc67631b89a3877a3d5da0f2dd
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-07-03 00:30:20 +0100

    Feat: mimes working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MemeGenerator from "./components/Memes/MemeGenerator";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+class App extends React.Component {
+  render() {
+    return <MemeGenerator />
+  }
+} export default App;
